be36ae07bb00f1eb4adadcdeeb5217c36214a19d
Alex Reyes <73624939+AlexReyesHuezo@users.noreply.github.com>
2023-12-11 21:26:48 -0600

    :poop:feat(get pokemon's features): add name, type and potential features to shearched pokemon

--- main.js
+++ main.js
@@ -13,13 +13,17 @@ const getPokemonData = (nombre) => {
 let id = 2
 document.querySelector('#app').innerHTML = `
 <section class="container d-flex flex-column align-items-center justify-content-center p-2 mt-5">
-<span class="synopsis-text input-group-text w-50 p-2" id="inputGroup-sizing-lg">Busca tu pokemón favorito</span>
+<span class="input-group-text w-50 p-2" id="inputGroup-sizing-lg">Busca tu pokemón favorito</span>
 <input type="text" id="poke-name" class="form-control w-50 m-2" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-<button class="btn btn-danger w-25 text-center p-4" id="search-pokemon">Buscar Pokem&oacute;n</button>
+<button class="btn btn-danger w-25 text-center p-4 m-2" id="search-pokemon">Buscar Pokem&oacute;n</button>
 
-</section>
-<div class="container" id="mostrar-pokemon">
-  <img src="" id="image-changer">
+<div class="card m-4 p-2" style="width: 18rem;" id="mostrar-pokemon">
+  <img src="" class="card-img-top" id="image-changer" alt="...">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item" id="mostrar-nombre"></li>
+    <li class="list-group-item" id="mostrar-tipo"></li>
+    <li class="list-group-item" id="mostrar-potencial">A third item</li>
+  </ul>
 </div>
 `
 const getPokemonId = (nombre) => {
@@ -27,14 +31,41 @@ const getPokemonId = (nombre) => {
     .then((response) => response.json())
     .then((data) => {
       document.querySelector('#image-changer').src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`
+      document.querySelector('#mostrar-nombre').innerHTML = `Nombre: ${data.name}`
     })
     .catch((error) => console.log(`API error: ${error}`))
 }
+
+// get pokemon type
+const getPokemonType = (nombre) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('mostrar-tipo').innerHTML = `Tipo: ${data.types[0].type.name}`
+      console.log(data.types[0].type.name)
+    })
+    .catch((error) => console.log(`API error: ${error}`))
+}
+
+// get pokemon potential
+const getPokemonPotential = (nombre) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('mostrar-potencial').innerHTML = `Potencia ${data.stats[0].base_stat}`
+      console.log(data.stats[0].base_stat)
+    })
+    .catch((error) => console.log(`API error: ${error}`))
+}
+
 
 document.querySelector('#search-pokemon').addEventListener('click', () =>{
   let pokemon = document.querySelector('#poke-name').value
   console.log(pokemon)
   getPokemonData(pokemon)
   getPokemonId(pokemon)
+  getPokemonType(pokemon)
+  getPokemonPotential(pokemon)
+
   
 })
